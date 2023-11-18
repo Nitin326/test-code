c6b88992f28eb7e9334c67d213dddfea80550064
nitin sain <nitin.sain@1finance.co.in>
2023-11-18 12:59:56 +0530

    Payment history added

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -5,6 +5,7 @@ import OtherIcon from './Assets/Images/other.svg'
 import FunIcon from './Assets/Images/fun.svg'
 import StyleIcon from './Assets/Images/style.svg'
 import TravelIcon from './Assets/Images/travel.svg';
+import ViewIcon from './Assets/Images/view.svg'
 import './Assets/payment.css'
 
 
@@ -13,21 +14,33 @@ const Payment = () => {
     <>
       <BottomAppBar />
       <div className='filter-box'>
-         <div className='filter-field'>
-            Food <img src={FoodIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
-         </div>
-         <div className='filter-field'>
-            Travel <img src={TravelIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
-         </div>
-         <div className='filter-field'>
-            Fun <img src={FunIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
-         </div>
-         <div className='filter-field'>
-            Life Style <img src={StyleIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
-         </div>
-         <div className='filter-field'>
-            Other <img src={OtherIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
-         </div>
+        <div className='filter-field'>
+          Food <img src={FoodIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+        </div>
+        <div className='filter-field'>
+          Travel <img src={TravelIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+        </div>
+        <div className='filter-field'>
+          Fun <img src={FunIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+        </div>
+        <div className='filter-field'>
+          Life Style <img src={StyleIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+        </div>
+        <div className='filter-field'>
+          Other <img src={OtherIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+        </div>
+      </div>
+      <div className='payment-history'>
+        <div className='payment-block'>
+          <div>Category</div>
+          <div>Date</div>
+          <div>$100</div>
+        </div>
+        <div className='payment-block'>
+          <div>Category</div>
+          <div>Date</div>
+          <div>$100</div>
+        </div>
       </div>
     </>
   )
