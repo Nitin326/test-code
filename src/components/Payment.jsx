6261f0e3b4170a77b2b991305160682bde01bb64
nitin sain <nitin.sain@1finance.co.in>
2023-11-18 12:40:53 +0530

    add icon on payment page

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import BottomAppBar from './BottomAppBar'
+import FoodIcon from './Assets/Images/food.svg'
+import OtherIcon from './Assets/Images/other.svg'
+import FunIcon from './Assets/Images/fun.svg'
+import StyleIcon from './Assets/Images/style.svg'
+import TravelIcon from './Assets/Images/travel.svg';
+import './Assets/payment.css'
+
 
 const Payment = () => {
   return (
     <>
       <BottomAppBar />
-      <div>Payment</div>
+      <div className='filter-box'>
+         <div className='filter-field'>
+            Food <img src={FoodIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+         </div>
+         <div className='filter-field'>
+            Travel <img src={TravelIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+         </div>
+         <div className='filter-field'>
+            Fun <img src={FunIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+         </div>
+         <div className='filter-field'>
+            Life Style <img src={StyleIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+         </div>
+         <div className='filter-field'>
+            Other <img src={OtherIcon} className='filter-icon' alt="food icon" height='20px' width='20px' />
+         </div>
+      </div>
     </>
   )
 }
